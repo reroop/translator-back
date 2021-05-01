e03251fff8aef0d079fb9a2d175b18de7b239d81
Reiko Roopärg <reroop@ttu.ee>
2021-05-01 10:49:30 +0300

    Initial commit

---
+++ translator/src/test/java/com/intern/translator/TranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intern.translator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
